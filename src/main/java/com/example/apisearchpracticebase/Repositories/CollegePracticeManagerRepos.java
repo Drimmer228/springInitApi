55769fd6fe5805db325420a5e7cb98d44a1272be
Dmitriy <55669105+Drimmer228@users.noreply.github.com>
2024-03-15 15:24:56 +0000

    test6

--- src/main/java/com/example/apisearchpracticebase/Repositories/CollegePracticeManagerRepos.java
+++
@@ -1,9 +0,0 @@
-package com.example.apisearchpracticebase.Repositories;
-
-import com.example.apisearchpracticebase.Models.CollegePracticeManager;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CollegePracticeManagerRepos extends CrudRepository<CollegePracticeManager, Long> {
-}
