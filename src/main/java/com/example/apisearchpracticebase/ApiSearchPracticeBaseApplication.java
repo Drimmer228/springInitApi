3623d905e687d74beda7e5b8403dae5f69c419f4
Dmitriy <55669105+Drimmer228@users.noreply.github.com>
2024-03-15 15:02:06 +0000

    test

--- src/main/java/com/example/apisearchpracticebase/ApiSearchPracticeBaseApplication.java
+++ src/main/java/com/example/apisearchpracticebase/ApiSearchPracticeBaseApplication.java
@@ -1,13 +1,22 @@
 package com.example.apisearchpracticebase;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.runApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class ApiSearchPracticeBaseApplication {
+class ApiSearchPracticeBaseApplication 
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiSearchPracticeBaseApplication.class, args);
+fun main(args: Array<String>){
+	runApplication<ApiSearchPracticeBaseApplication>(*args);
+}
+
+@RestController
+class ApiSearchPracticeBaseController{
+	@GetMapping("hello")
+	fun hello() : String{
+		return "Hello render"
 	}
-
 }
